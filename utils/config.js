87568613e39388a00e5c0ddb7859561026cfd3c9
ophid1an <dermentzoglou.ioannis@gmail.com>
2018-01-10 02:54:07 +0200

    Update for new dataset

---
+++ utils/config.js
@@ -0,0 +1,28 @@
+exports.dbURL = 'mongodb://localhost/mm-dbs-project';
+
+exports.algorithmsSupported = [
+  {
+    name: 'gist',
+    len: 512,
+  },
+  {
+    name: 'hsvHist',
+    len: 343,
+  },
+  {
+    name: 'hsvHistLayout',
+    len: 375,
+  },
+  {
+    name: 'rgbHist',
+    len: 343,
+  },
+  {
+    name: 'sfta',
+    len: 42,
+  },
+  {
+    name: 'sift',
+    len: 100,
+  },
+];
